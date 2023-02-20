bfacf67f76edae9e910a1886824ddea27fde6634
Franco Borgiani <92275958+francoborgiani@users.noreply.github.com>
2023-02-20 19:58:18 -0300

    fixing omega 3 & Blueberry Oatmeal text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.header("🥣Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞Avocado Toast")
