3bdceea6c6b1d1503e291369e046f7f79653e26d
Franco Borgiani <92275958+francoborgiani@users.noreply.github.com>
2023-02-21 10:28:26 -0300

    fruityvice json advice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 
 import streamlit
-import pandas as pd
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -12,6 +11,8 @@ streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# Fruits picker section
+import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -21,3 +22,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+#Fruityvice Advice section
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.text(fruityvice_response.json())
