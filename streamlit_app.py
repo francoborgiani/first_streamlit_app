0b577878db778c5f209b9f805bd9b0ccf1c5e3ba
Franco Borgiani <92275958+francoborgiani@users.noreply.github.com>
2023-02-21 10:29:10 -0300

    add advice section header

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#Fruityvice Advice section
+# Fruityvice Advice section
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
+# Fruityvice visual content:
+streamlit.header("Fruityvice Fruit Advice!")
 streamlit.text(fruityvice_response.json())
