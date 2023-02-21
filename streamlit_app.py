37a9e135162186f9bd84f321e32c986877fe0388
Franco Borgiani <92275958+francoborgiani@users.noreply.github.com>
2023-02-21 11:59:00 -0300

    add simple error validation for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,16 +29,24 @@ streamlit.dataframe(fruits_to_show)
 # Fruityvice section:
 streamlit.header("Fruityvice Fruit Advice!")
 
+try:
+  fruit_choice = streamlit.text_input("What fruit would you like information about?", "Kiwi")
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+    # Normalize data using pandas
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+  streamlit.error()
+
 # Fruityvice input
-fruit_choice = streamlit.text_input("What fruit would you like information about?", "Kiwi")
 streamlit.write("The user entered: " + fruit_choice)
 
 # Fruityvice requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# Normalize data using pandas
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
